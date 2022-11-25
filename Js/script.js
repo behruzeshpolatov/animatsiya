4bbaa46d9634ec97570d2db55a28178cda09b23c
behruzeshpolatov <116355462+behruzeshpolatov@users.noreply.github.com>
2022-11-25 19:08:35 +0500

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,19 @@
+const colorFunc=() => {
+   var colorPicker = document.getElementById("inputColor").value;
+   document.body.style.backgroundColor=colorPicker;
+   document.getElementById("inputText").value=colorPicker;
+}
+const copy=()=>{
+    const hexColor=document.getElementById("inputText");
+    const demo=document.getElementById("demo");
+    navigator.clipboard.writeText(hexColor.value);
+    demo.innerHTML="Saqlandi!"
+    demo.style.opacity="1"
+}
+
+
+if("serviceWorker" in navigator){
+    window.addEventListener("load", function(){
+        navigator.serviceWorker.register("/serviceWorke.js")
+    })
+}
